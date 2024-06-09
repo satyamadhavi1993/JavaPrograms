3fda332690ee869e1e830c7b9e94bbf68901617e
Satya Madhavi <159613485+satyamadhavi1993@users.noreply.github.com>
2024-06-08 19:52:04 -0700

    Update few programs

--- JavaPrograms/src/ArrayListPrograms/Max_Three.java
+++ JavaPrograms/src/ArrayListPrograms/Max_Three.java
@@ -1,3 +1,5 @@
+package ArrayListPrograms;
+
 import java.util.ArrayList;
 import java.util.List;
 
--- JavaPrograms/src/ExceptionsExamples/ExOfNullPointerException.java
+++ JavaPrograms/src/ExceptionsExamples/ExOfNullPointerException.java
@@ -5,7 +5,5 @@ public class ExOfNullPointerException {
 	public static void main(String[] args) {
 		String s = null;
 		System.out.println(s.length());
-
 	}
-
 }
