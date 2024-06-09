3f5390866142e85093ab9ecb05e7fd7869079fc9
Satya Madhavi <159613485+satyamadhavi1993@users.noreply.github.com>
2024-06-08 19:39:11 -0700

    Update few programs

--- JavaPrograms/src/StringManipulations/Find.java
+++ JavaPrograms/src/StringManipulations/Find.java
@@ -1,16 +1,6 @@
 package StringManipulations;
 
 public class Find {
-    // // String name = "89m%a))dad>--h@@a$vi";
-    // char[] ch = name.toCharArray();
-    // int a = 0;
-    // for (char c:ch) {
-    //     if(Character.isAlphabetic(c)) {
-    //         a++;
-    //     } else if(Character.isDigit(c)) {
-
-    //     }
-    // }
     public static void main(String[] args) {
         String name = "89m%a))dad>--h@@a$vi";
         int l = name.length();
