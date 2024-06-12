a5eef4dca95c47ceeb2badf17effce93ac20be90
Satya Madhavi <159613485+satyamadhavi1993@users.noreply.github.com>
2024-06-12 00:05:34 -0700

    Add MergeSort program

--- JavaPrograms/src/ArraysManipulations/MergeSort.java
+++ JavaPrograms/src/ArraysManipulations/MergeSort.java
@@ -2,37 +2,58 @@ package ArraysManipulations;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-public class MergeSort {
+public class MergeSort_Inprogress {
     public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<Integer>(
-				Arrays.asList(344, 523, 542, 11, 3, 8, 67, 35, 87, 14, 76, 12, 35, 11, 74, 423, 239));
+        List<Integer> numbers = new ArrayList<Integer>(
+				Arrays.asList(344, 523, -542, 11, 3, 8, 67, -35, 87, 14, 76, -12, 35, 11, 74, -423, 239, 0));
+        System.out.println(mergeSort(numbers));
         
     }
-    public static void mergeSort(ArrayList<Integer> numbers) {
-        int len = numbers.size();
-        int left = 0;
-        int right = numbers.size() - 1;
-        int mid = (left+right)/2;
-        
-
-        
-
+    public static List<Integer> mergeSort(List<Integer> numbers) {
+        if (numbers.size() <= 1) {
+            return numbers;
+        }
+        return divide(numbers, 0, numbers.size() - 1);
     }
 
-    public static void merge(ArrayList<Integer> numbers, int left, int right) {
-        int len = numbers.size();
-        ArrayList<Integer>  leftArray = new ArrayList<Integer>();
-        ArrayList<Integer>  rightArray = new ArrayList<Integer>();
-        int mid = (left+right)/2;
-        for(Integer num:numbers) {
-            if(num <= mid) {
-                leftArray.add(num);
-            } else {
-                rightArray.add(num);
+    public static List<Integer> divide(List<Integer> numbers, int low, int high) {
+        if (low == high) {
+            return new ArrayList<Integer>(){{
+                add(numbers.get(low));
+            }};
+        }
+        int mid = (low + high)/2;
+        List<Integer> leftArray = divide(numbers, low, mid);
+        List<Integer> rightArray = divide(numbers, mid + 1, high);
+        return merge(leftArray, rightArray);
+    }
+
+    public static List<Integer> merge(List<Integer> leftArray, List<Integer> rightArray) {
+        List<Integer> sortedList = new ArrayList<Integer>();
+        int pointer1 = 0, pointer2 = 0;
+        int leftArraySize = leftArray.size();
+        int rightArraySize = rightArray.size();
+        while (pointer1 < leftArraySize || pointer2 < rightArraySize) {
+            if (pointer2 == rightArraySize) {
+                sortedList.add(leftArray.get(pointer1));
+                pointer1++;
+            }
+            else if (pointer1 == leftArraySize) {
+                sortedList.add(rightArray.get(pointer2));
+                pointer2++;
+            }
+            else if (leftArray.get(pointer1) <= rightArray.get(pointer2)) {
+                sortedList.add(leftArray.get(pointer1));
+                pointer1++;
+            }
+            else if (leftArray.get(pointer1) > rightArray.get(pointer2)) {
+                sortedList.add(rightArray.get(pointer2));
+                pointer2++;
             }
         }
-    }
 
-    
+        return sortedList;
+    }
 }
